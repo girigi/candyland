4eaeb79d32702c6998680641b9ef3132be4a69cc
Grigoriy Popov <grigoriy_popov.07@mail.ru>
2026-06-02 19:48:03 +0300

    Переход на django 5

--- CandyLand/settings.py
+++ CandyLand/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5&lmi&w*ytv4pvyf3ukta47kh!w5#y71d0z@lxac5usm63nmik'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
